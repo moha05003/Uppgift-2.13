dacd96f45d7225a25e3fb5814677940f5af83c34
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:29:08 +0200

    Add project files.

---
+++ Uppgift 2.13/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket lön för Ali?");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket lön för Naser?");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket lön för Mohammad?");
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("medellönen för personalen är ");
+            Console.WriteLine("_____________________________");
+
+
+            Console.WriteLine((x+y+z)/3 );
+            Console.WriteLine("_____________________________");
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
+
